2f0c8dce2f4d6dedf5ab833767d09c7472b75794
malavikaap <85464603+ashiqu-ali@users.noreply.github.com>
2022-12-18 10:19:43 +0530

    push

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,65 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta: # all thing under this class become the object of above class.
         model = Profile
         fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Wallet
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+
+class Total_HistorySerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Total_History
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class NewcardSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Newcard
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class LibrarySerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Library
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class CartSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Cart
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Store
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class CanteenSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Canteen
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class BusSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Bus
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class FeesSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Fees
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class Fee_typeSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Fee_type
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class PaylaterSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Paylater
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class SettingsSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Settings
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Profile
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class OthersSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Others
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
+class IgoalSerializer(serializers.ModelSerializer):
+    class Meta: # all thing under this class become the object of above class.
+        model = Igoal
+        fields = '__all__'   # for needed field only ('fullname','mobile'). __all__ is means all the fields
--- api/viewsets.py
+++ api/viewsets.py
@@ -6,6 +6,44 @@ class ProfileViewset(viewsets.ModelViewSet):
     queryset =  models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
 
+class WalletViewset(viewsets.ModelViewSet):
+    queryset =  models.Wallet.objects.all()
+    serializer_class = serializers.WalletSerializer
+class Total_HistoryViewset(viewsets.ModelViewSet):
+    queryset =  models.Total_History.objects.all()
+    serializer_class = serializers.Total_HistorySerializer
+class NewcardViewset(viewsets.ModelViewSet):
+    queryset =  models.Newcard.objects.all()
+    serializer_class = serializers.NewcardSerializer
+class LibraryViewset(viewsets.ModelViewSet):
+    queryset =  models.Library.objects.all()
+    serializer_class = serializers.LibrarySerializer
+class CartViewset(viewsets.ModelViewSet):
+    queryset =  models.Cart.objects.all()
+    serializer_class = serializers.CartSerializer
+class StoreViewset(viewsets.ModelViewSet):
+    queryset =  models.Store.objects.all()
+    serializer_class = serializers.StoreSerializer
+class BusViewset(viewsets.ModelViewSet):
+    queryset =  models.Bus.objects.all()
+    serializer_class = serializers.BusSerializer
+class Fee_typeViewset(viewsets.ModelViewSet):
+    queryset =  models.Fee_type.objects.all()
+    serializer_class = serializers.Fee_typeSerializer
+class PaylaterViewset(viewsets.ModelViewSet):
+    queryset =  models.Paylater.objects.all()
+    serializer_class = serializers.PaylaterSerializer
+class SettingsViewset(viewsets.ModelViewSet):
+    queryset =  models.Settings.objects.all()
+    serializer_class = serializers.SettingsSerializer
+class OthersViewset(viewsets.ModelViewSet):
+    queryset =  models.Others.objects.all()
+    serializer_class = serializers.OthersSerializer
+class IgoalViewset(viewsets.ModelViewSet):
+    queryset =  models.Igoal.objects.all()
+    serializer_class = serializers.IgoalSerializer
+
+
 # this viewset automatically make some functions which are:
 # create()
 # retrieve() => only retrive one data
