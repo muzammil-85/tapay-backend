4d881cd9ec86de51038e9b0ce96a92a9cecddf85
malavikaap <85464603+ashiqu-ali@users.noreply.github.com>
2022-12-18 11:00:41 +0530

    push

--- restapi/router.py
+++ restapi/router.py
@@ -1,9 +1,18 @@
-from api.viewsets import ProfileViewset
+from api.viewsets import *
 from  rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('profile',ProfileViewset)
+router.register('Wallet',WalletViewset)
+router.register('Total_History',Total_HistoryViewset)
+router.register('Newcard',NewcardViewset)
+router.register('Library',LibraryViewset)
+router.register('Cart',CartViewset)
+router.register('Store',StoreViewset)
+router.register('Bus',BusViewset)
+router.register('Fee_type',Fee_typeViewset)
+router.register('Paylater',PaylaterViewset)
+router.register('Settings',SettingsViewset)
+router.register('Others',OthersViewset)
+router.register('Igoal',IgoalViewset)
 
-# url will be like this :
-#   localhost:8000/api/employee/
-# methods are GET , POST , PUT , DELETE
--- restapi/urls.py
+++ restapi/urls.py
@@ -17,9 +17,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path as url
 from .router import router
+# from django.conf.urls import re_path
+from blockchain import views
+from blockchain.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/',include(router.urls)),
+     url('^get_chain$', views.get_chain, name="get_chain"),
+    url('^mine_block$', views.mine_block, name="mine_block"),
+    url('^add_transaction$', views.add_transaction, name="add_transaction"), #New
+    url('^is_valid$', views.is_valid, name="is_valid"), #New
+    url('^connect_node$', views.connect_node, name="connect_node"), #New
+    url('^replace_chain$', views.replace_chain, name="replace_chain"), #New
 ]
